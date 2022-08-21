4463235bcfbd2d3cf5e5db171494640f0747619b
jmpfbmx <jmpf.bmx@gmail.com>
2022-08-21 20:36:09 +0200

    mt6592-common: Import patches
    Co-authored-by: fire855 <thefire855@gmail.com>
    Co-authored-by: Cesar <cesar.maximo@gmail.com>
    Co-authored-by: DerTeufel <dominik-kassel@gmx.de>
    Co-authored-by: danielhk <daniel.p6800@gmail.com>
    Co-authored-by: DeckerSU <deckersu@protonmail.com>
    Co-authored-by: Dmitry Smirnov <divis1969@gmail.com>
    Change-Id: I7a53b75687c634831fad243558bb33a2b6c12c38

---
+++ patches/apply.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="external/icu external/wpa_supplicant_8 frameworks/av frameworks/base frameworks/opt/telephony packages/apps/Snap system/core system/netd"
+
+# red + nocolor
+RED='\033[0;31m'
+NC='\033[0m'
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+    echo -e "\n${RED}Applying ${NC}$dir ${RED}patches...${NC}\n"
+	git apply -v $rootdirectory/device/bq/mt6592-common/patches/$dir/*.patch
+done
+
+# -----------------------------------
+echo -e "Done !\n"
+cd $rootdirectory
---
+++ patches/uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rootdirectory="$PWD"
+dirs="external/icu external/wpa_supplicant_8 frameworks/av frameworks/base frameworks/opt/telephony packages/apps/Snap system/core system/netd"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Cleaning $dir patches..."
+	git checkout -- . && git clean -df
+done
+
+echo "Done!"
+cd $rootdirectory
