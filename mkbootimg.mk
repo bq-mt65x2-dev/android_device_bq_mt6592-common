ffb86a00a36529eaeaa2b47685c0beb3dfd5b6d8
R0rt1z2 <rogerortizleal@gmail.com>
2022-08-21 23:31:58 +0200

    mt6592-common: apply MTK header to the kernel as well
    * Also update the copyright year.

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2013 The Android Open-Source Project
+# Copyright (C) 2022 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +14,19 @@
 # limitations under the License.
 #
 
-# Credits go to bgcngm, chrmhoffmann & BSydz
-
-# Can this be done in bash?
+# can this be done in bash? credits go to bgcngm
 define make_header
   perl -e 'print pack("a4 L a32 a472", "\x88\x16\x88\x58", $$ARGV[0], $$ARGV[1], "\xFF"x472)' $(1) $(2) > $(3)
 endef
+
+# this is overriding targets from build/core/Makefile
+$(INSTALLED_KERNEL_TARGET).mtk.header: $(INSTALLED_KERNEL_TARGET)
+	size=$$($(call get-file-size,$(INSTALLED_KERNEL_TARGET))); \
+		$(call make_header, $$((size)), "KERNEL", $@)
+$(INSTALLED_KERNEL_TARGET).mtk: $(INSTALLED_KERNEL_TARGET).mtk.header
+	$(call pretty,"Adding MTK header to kernel.")
+	cat $(INSTALLED_KERNEL_TARGET).mtk.header $(INSTALLED_KERNEL_TARGET) \
+		> $@
 
 $(INSTALLED_RAMDISK_TARGET).mtk.header: $(INSTALLED_RAMDISK_TARGET)
 	size=$$($(call get-file-size,$(INSTALLED_RAMDISK_TARGET))); \
@@ -29,6 +36,13 @@ $(INSTALLED_RAMDISK_TARGET).mtk: $(INSTALLED_RAMDISK_TARGET).mtk.header
 	cat $(INSTALLED_RAMDISK_TARGET).mtk.header $(INSTALLED_RAMDISK_TARGET) \
 		> $@
 
+$(PRODUCT_OUT)/recovery_kernel.mtk.header: $(recovery_kernel)
+	size=$$($(call get-file-size,$(recovery_kernel))); \
+		$(call make_header, $$((size)), "KERNEL", $@)
+$(PRODUCT_OUT)/recovery_kernel.mtk: $(PRODUCT_OUT)/recovery_kernel.mtk.header
+	$(call pretty,"Adding MTK header to recovery kernel.")
+	cat $(PRODUCT_OUT)/recovery_kernel.mtk.header $(recovery_kernel) > $@
+
 $(recovery_ramdisk).mtk.header: $(recovery_ramdisk)
 	size=$$($(call get-file-size,$(recovery_ramdisk))); \
 		$(call make_header, $$((size)), "RECOVERY", $@)
@@ -37,11 +51,11 @@ $(recovery_ramdisk).mtk:  $(MKBOOTIMG) $(recovery_ramdisk).mtk.header
 	cat $(recovery_ramdisk).mtk.header $(recovery_ramdisk) > $@
 
 INTERNAL_MTK_BOOTIMAGE_ARGS := \
-		--kernel $(INSTALLED_KERNEL_TARGET) \
+		--kernel $(INSTALLED_KERNEL_TARGET).mtk \
 		--ramdisk $(INSTALLED_RAMDISK_TARGET).mtk
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG)\
-		$(INSTALLED_RAMDISK_TARGET).mtk $(INSTALLED_KERNEL_TARGET)
+		$(INSTALLED_RAMDISK_TARGET).mtk $(INSTALLED_KERNEL_TARGET).mtk
 	$(call pretty,"Target boot image: $@")
 	$(MKBOOTIMG) $(INTERNAL_MTK_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) \
 		--output $@
@@ -50,11 +64,11 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG)\
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 INTERNAL_MTK_RECOVERYIMAGE_ARGS := \
-		--kernel $(recovery_kernel) \
+		--kernel $(INSTALLED_KERNEL_TARGET).mtk \
 		--ramdisk $(recovery_ramdisk).mtk
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recovery_ramdisk).mtk $(recovery_kernel)
+		$(recovery_ramdisk).mtk $(INSTALLED_KERNEL_TARGET).mtk
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(MKBOOTIMG) $(INTERNAL_MTK_RECOVERYIMAGE_ARGS) \
 		$(BOARD_MKBOOTIMG_ARGS) --output $@
